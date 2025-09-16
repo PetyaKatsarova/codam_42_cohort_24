5b141b424264ee20ac57ef1569a9085708c3bb54
Petya Katsarova <pskpetya@gmail.com>
2025-09-16 18:11:38 +0200

    work in progress: cpp04

--- pr_18_cpp04_rank_4/ex00/main.cpp
+++ pr_18_cpp04_rank_4/ex00/main.cpp
@@ -7,42 +7,43 @@
 #include <string>
 
 int main() {
-const Animal* meta = new Animal();
-const Animal* j = new Dog();
-const Animal* i = new Cat();
-std::cout << j->getType() << " " << std::endl;
-std::cout << i->getType() << " " << std::endl;
-i->makeSound(); //will output the cat sound!
-j->makeSound();
-meta->makeSound();
+// const Animal* meta = new Animal();
+// const Animal* j = new Dog();
+// const Animal* i = new Cat();
+// std::cout << j->getType() << " " << std::endl;
+// std::cout << i->getType() << " " << std::endl;
+// i->makeSound(); //will output the cat sound!
+// j->makeSound();
+// meta->makeSound();
 
-std::cout << "***** Wrong cat ******" << std::endl;
-const WrongAnimal* wa = new WrongAnimal();
-const WrongAnimal* wc = new WrongCat();
-std::cout << wa->getType() << " " << std::endl;
-std::cout << wc->getType() << " " << std::endl;
-wa->makeSound(); //will output the cat sound!
-wc->makeSound();
+// std::cout << "***** Wrong cat ******\n" << std::endl;
+// const WrongAnimal* wa = new WrongAnimal();
+// const WrongAnimal* wc = new WrongCat();
+// std::cout << wa->getType() << " " << std::endl;
+// std::cout << wc->getType() << " " << std::endl;
+// wa->makeSound(); //will output the cat sound!
+// wc->makeSound();
 
-std::cout << "**** EXTRA TESTS ****" << std::endl;
-std::cout << "=== Correct tests ===" << std::endl;
-const Animal* a = new Cat();
-a->makeSound();
+// std::cout << "**** EXTRA TESTS ****\n" << std::endl;
+// std::cout << "=== Correct tests ===" << std::endl;
+// const Animal* a = new Cat();
+// a->makeSound();
 
-Cat c1;
-Animal* ac = &c1;
-ac->makeSound(); 
+// Cat c1;
+// Animal* ac = &c1;
+// ac->makeSound(); 
 
-Animal* zoo[2];
-zoo[0] = new Dog();
-zoo[1] = new Cat();
-for (int k = 0; k < 2; ++k)
-    zoo[k]->makeSound();
+// Animal* zoo[2];
+// zoo[0] = new Dog();
+// zoo[1] = new Cat();
+// for (int k = 0; k < 2; ++k)
+//     zoo[k]->makeSound();
 
 Dog d1;
-Animal baseDog = d1;
-baseDog.makeSound(); // Dog sound (if virtual), else Animal sound
+Animal baseDog = d1; // object slicing: copies only the Animal part of dog1
+baseDog.makeSound(); // always calls Animal
 
-
+Animal& baseDog2 = d1;
+baseDog2.makeSound(); // dog
 return 0;
 }
