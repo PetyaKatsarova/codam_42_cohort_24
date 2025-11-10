f198f89f0524ce360a0305c797e3bd461d44c275
Petya Katsarova <pskpetya@gmail.com>
2025-11-10 20:04:12 +0100

    added diff solution for Intern.cpp

--- pr_22_cpp05_rank_5/ex03/Intern.cpp
+++ pr_22_cpp05_rank_5/ex03/Intern.cpp
@@ -28,19 +28,55 @@ Intern& Intern::operator=(const Intern& other) {
     return *this;
 }
 
+// AForm* Intern::makeForm(const std::string &name, const std::string &target) {
+//     if (name == "robotomy request") {
+//         std::cout << "Intern creates " << name << std::endl;
+//         return new RobotomyRequestForm(target);
+//     }
+//     if (name == "presidential pardon") {
+//         std::cout << "Intern creates " << name << std::endl;
+//         return new PresidentialPardonForm(target);
+//     }
+//     if (name == "schrubbery creation") {
+//         std::cout << "Intern creates " << name << std::endl;
+//         return new SchrubberyCreationForm(target);
+//     }
+//     std::cerr << "Error: Form '" << name << "' doesn't exist" << std::endl;
+//     return NULL;
+// }
+
+static AForm* createRobotomy(const std::string &target) {
+    return new RobotomyRequestForm(target);
+}
+
+static AForm* createPresidential(const std::string &target) {
+    return new PresidentialPardonForm(target);
+}
+
+static AForm* createSchrubbery(const std::string &target) {
+    return new SchrubberyCreationForm(target);
+}
+
 AForm* Intern::makeForm(const std::string &name, const std::string &target) {
-    if (name == "robotomy request") {
-        std::cout << "Intern creates " << name << std::endl;
-        return new RobotomyRequestForm(target);
+    const std::string formNames[3] = {
+        "robotomy request",
+        "presidential pardon",
+        "schrubbery creation"
+    };
+    
+    AForm* (*creators[3])(const std::string &) = {
+        &createRobotomy,
+        &createPresidential,
+        &createSchrubbery
+    };
+    
+    for (int i = 0; i < 3; i++) {
+        if (name == formNames[i]) {
+            std::cout << "Intern creates " << name << std::endl;
+            return creators[i](target);
+        }
     }
-    if (name == "presidential pardon") {
-        std::cout << "Intern creates " << name << std::endl;
-        return new PresidentialPardonForm(target);
-    }
-    if (name == "schrubbery creation") {
-        std::cout << "Intern creates " << name << std::endl;
-        return new SchrubberyCreationForm(target);
-    }
+    
     std::cerr << "Error: Form '" << name << "' doesn't exist" << std::endl;
     return NULL;
 }
