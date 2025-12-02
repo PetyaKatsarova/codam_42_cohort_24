5ff3892cb7bc43cdf6bcde7de0ec8b60789b693b
Petya Katsarova <pskpetya@gmail.com>
2025-12-02 21:23:10 +0100

    addec cpp07 ex01

--- pr_24_cpp07_rank_5/ex01/main.cpp
+++ pr_24_cpp07_rank_5/ex01/main.cpp
@@ -1,31 +1,61 @@
 #include "iter.hpp"
 #include <iostream>
+#include <string>
 
-void printInt(int& n) {
-    std::cout << n << " ";
-}
+void printIntConst(const int& n) { std::cout << n << " "; }
 
 void doubleVal(int& n) { n *= 2; }
 
+void printStr(std::string& str) { std::cout << str << " "; }
+
+void printConstStr(const std::string& str) { std::cout << str << " "; }
+
+void strCapitalizeFirst(std::string& str) { 
+    if (!str.empty())
+        str[0] = std::toupper(str[0]);
+}
+
+template<typename T>
+void incrementTemplate(T& val) { ++val; }
+
+template<typename T>
+void printTemplate (const T& val) { std::cout << val << " "; }
+
 int main() {
-    std::cout << "=== Test 1: Print Integer Array ===" << std::endl;
     int intArr[] = {1, 2, 3, 4, 5};
     size_t intLen = 5;
-    
-    std::cout << "Original: ";
-    iter(intArr, intLen, printInt);
-    std::cout << std::endl;
 
-    std::cout << "\n=== Test 2: Modify Integer Array (Double the int in arr) ===" << std::endl;
+    std::cout << "\n=== Test 1: Modify Integer Array (Double the int in arr) and print const int arr ===" << std::endl;
     std::cout << "Before: ";
-    iter(intArr, intLen, printInt);
+    iter(intArr, intLen, printIntConst);
     std::cout << std::endl;
     
     iter(intArr, intLen, doubleVal);
     
     std::cout << "After:  ";
-    iter(intArr, intLen, printInt);
+    // iter(intArr, intLen, printTemplate<int>);
+    iter(intArr, intLen, printIntConst);
     std::cout << std::endl;
 
+    std::cout << "\n-------------- TEST STRINGS ---------------------\n";
+    const std::string constStrArr[] = {"hello", "world", "we",  "come", "in",  "peace"};
+    std::string strArr[] = {"hello", "world", "we",  "come", "in",  "peace"};
+
+    std::cout << "\n-- print const str arr --\n";
+    // iter(constStrArr, 6, printStr); // for testing showing err
+    iter(constStrArr, 6, printConstStr); 
+    std::cout << "\n-- capitalize and print str arr --\n";
+    iter(strArr, 6, strCapitalizeFirst);
+    iter(strArr, 6, printStr); 
+    std::cout << "\n";
+
+    std::cout << "\n=== Test 5: Multiple Increments ===\n" << std::endl;
+    float multiArr[] = {0.0f, 10.10f, 20.20f, 30.30f};
+    std::cout << "Original: ";
+    iter(multiArr, 4, printTemplate<float>);
+    std::cout << "\nIncrement: \n";
+    iter(multiArr, 4, incrementTemplate<float>);
+    iter(multiArr, 4, printTemplate<float>);
+    std::cout << "\n";
     return 0;
 }
