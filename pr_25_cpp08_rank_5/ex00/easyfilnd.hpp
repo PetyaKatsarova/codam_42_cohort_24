bc54a42a17b723217643a15530a35e5fc9e436e6
Petya Katsarova <pekatsar@f1r2s16.codam.nl>
2025-12-05 13:39:54 +0100

    added ex00

---
+++ pr_25_cpp08_rank_5/ex00/easyfilnd.hpp
@@ -0,0 +1,19 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+
+/**
+Assuming T is a container of integers, this function has to find the first occurrence
+of the second parameter in the first parameter.
+If no occurrence is found, you can either throw an exception or return an error value
+of your choice. If you need some inspiration, analyze how standard containers behave.
+Of course, implement and turn in your own tests to ensure everything works as expected.
+You don’t have to handle associative containers.
+*/
+template<typename T>
+void easyfind(T val, int num) {
+
+}
+
+#endif
